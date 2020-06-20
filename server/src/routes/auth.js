3382b17a2de5498d59519e384269ecd8b0f5f027
PaulAduGyamfi <pjadu7@gmail.com>
2020-06-20 15:23:47 -0400

    user login logic

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -46,49 +46,61 @@ router.post('/signup', (req,res) => {
   .catch
 })
 
+router.post('/signin',(req,res) => {
+   const {username,email,password} = req.body
+   if(!username && !email){
+      return res.status(422).json({error:"A username or email is required to login"})
+   }
+   if( ((!username && email) && !password ) || ((username && !email) && !password ) ){
+      return res.status(422).json({error:"Please provide password"})
+   }
+
+      if(username && !email){
+         User.findOne({username:username})
+         .then(savedUser => {
+            if(!savedUser){
+               return res.status(422).json({err:"Invalid username or password"})
+            }
+            bcrypt.compare(password, savedUser.password)
+            .then(passwordMatch => {
+               if(passwordMatch){
+                  res.json({message:"Successfully logged in"})
+               }
+               else{
+                  return res.json({error:"Invalid username or password"})
+               }
+            })
+            .catch(err => {
+               console.log(err)
+            })
+         })
+      }
+      else if(!username && email){
+         User.findOne({username:username})
+         .then(savedUser => {
+            if(!savedUser){
+               return res.status(422).json({err:"Invalid email or password"})
+            }
+            bcrypt.compare(password, savedUser.password)
+            .then(passwordMatch => {
+               if(passwordMatch){
+                  res.json({message:"Successfully logged in"})
+               }
+               else{
+                  return res.json({error:"Invalid email or password"})
+               }
+            })
+            .catch(err => {
+               console.log(err)
+            })
+         })
+      }
+   
+   
+   
+})
+
 
 module.exports = router
 
 
-
-/*
-
-
-
- User.findOne({email:email})
-   .then(savedUser => {
-      if(savedUser){
-         return res.status(422).json({
-            error:"An account with that email already exists"
-         })
-      }
-   })
-   .then(() => {
-      User.findOne({username:username})
-      .then(savedUser => {
-         if(savedUser){
-            return res.status(422).json({
-               error:"An account with that username already exists"
-            })
-         }
-         const user = new User({
-            firstname,
-            lastname,
-            username,
-            email,
-            password
-         })
-      
-         user.save()
-         .then(user => {
-            res.json({
-               message:"Great you're all signed up"
-            })
-         })
-         .catch
-      })
-      .catch
-      
-   })
-   
- */
