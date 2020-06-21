8e1b5abc5c343f0d404f5a5bd6e458d75db93c7e
PaulAduGyamfi <pjadu7@gmail.com>
2020-06-21 10:29:55 -0400

    styling user profile

--- client/src/components/views/Profile/ProfileCard.js
+++ client/src/components/views/Profile/ProfileCard.js
@@ -3,7 +3,26 @@ import './profileStyles/ProfileCard.scss'
 
 const ProfileCard = () => {
     return(
-        <div className="profilecardWrap">Profile Card</div>
+        <div className="profilecardWrap"> 
+            <div className="profileBanner">
+                    BANNER
+            </div>
+            <div className="profileInfo">
+                <div className="profileInfoWrap">
+                    <div className="profileInfoTop">
+                        <div className="profilePicture"></div>
+                        <div className="editProfileButton"><button>Edit profile</button></div>
+                    </div>
+                    <div className="profileInfoBottom">
+                            <div className="profileUsername">Kingjames</div>
+                            <div className="profileStats">
+                                <div className="profileFollowers"><span id="followers">1.2M</span> Followers</div>
+                                <div className="profileFolling"><span id="following">100</span> Following</div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default ProfileCard
