7409f6408802fcb6f1e4b2ab4863f7bfae73d9b2
PaulAduGyamfi <pjadu7@gmail.com>
2020-06-21 17:31:41 -0400

    stlyling

--- client/src/components/views/Explore/PostBox.js
+++ client/src/components/views/Explore/PostBox.js
@@ -14,7 +14,6 @@ const PostBox = () =>{
     
         // Calculate the height
         var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
-                     + parseInt(computed.getPropertyValue('padding-top'), 10)
                      + field.scrollHeight
                      + parseInt(computed.getPropertyValue('padding-bottom'), 10)
                      + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
