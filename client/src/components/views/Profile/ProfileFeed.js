90184a62790d29b7a4e49439a2aa47e01d06022c
PaulAduGyamfi <pjadu7@gmail.com>
2020-06-21 12:22:15 -0400

    styling user profile

--- client/src/components/views/Profile/ProfileFeed.js
+++ client/src/components/views/Profile/ProfileFeed.js
@@ -1,9 +1,177 @@
 import React from 'react'
 import './profileStyles/ProfileFeed.scss'
+import pic from "../viewsStyles/imgs/lbj.jpg";
+import {DownOutlined,HeartOutlined,MessageOutlined} from "@ant-design/icons";
 
 const ProfileFeed = () => {
     return(
-        <div className="profilefeedContainer">Profile Feed</div>
+        <div className="profilefeedContainer">
+
+            <div className="feedCard">
+                <div className="feedCard-Left">
+                    <div className="profilePicture" style={{backgroundImage: `url(${pic})`, backgroundPosition: "50% 50%", backgroundSize: "cover"}}></div>
+                </div>
+                <div className="feedCard-Right">
+                    <div className="feedCard-header">
+                            <div className="feedCard-info">
+                                    <div className="feedCardAuthor Name">Lebron James</div>
+                                    <div className="feedCardAuthor Tag">@kingjames</div>
+                            </div>
+                            <div className="feedCard-arrow">
+                                    <DownOutlined />
+                            </div>
+                    </div>
+                    <div className="feedCard-body">
+                        We’ve teamed up with @solefed to giveaway a brand new Apple Watch Series 3 (GPS, 42mm) Space Gray Aluminium Case with Black Sport Band to ONE lucky winner. 
+                    </div>
+                    <div className="feedCard-actions">
+                        <HeartOutlined />
+                        <MessageOutlined />
+                    </div>
+                </div>
+            </div>
+            <div className="feedCard">
+                <div className="feedCard-Left">
+                    <div className="profilePicture" style={{backgroundImage: `url(${pic})`, backgroundPosition: "50% 50%", backgroundSize: "cover"}}></div>
+                </div>
+                <div className="feedCard-Right">
+                    <div className="feedCard-header">
+                            <div className="feedCard-info">
+                                    <div className="feedCardAuthor Name">Lebron James</div>
+                                    <div className="feedCardAuthor Tag">@kingjames</div>
+                            </div>
+                            <div className="feedCard-arrow">
+                                    <DownOutlined />
+                            </div>
+                    </div>
+                    <div className="feedCard-body">
+                        We’ve teamed up with @solefed to giveaway a brand new Apple Watch Series 3 (GPS, 42mm) Space Gray Aluminium Case with Black Sport Band to ONE lucky winner. 
+                    </div>
+                    <div className="feedCard-actions">
+                        <HeartOutlined />
+                        <MessageOutlined />
+                    </div>
+                </div>
+            </div>
+            <div className="feedCard">
+                <div className="feedCard-Left">
+                    <div className="profilePicture" style={{backgroundImage: `url(${pic})`, backgroundPosition: "50% 50%", backgroundSize: "cover"}}></div>
+                </div>
+                <div className="feedCard-Right">
+                    <div className="feedCard-header">
+                            <div className="feedCard-info">
+                                    <div className="feedCardAuthor Name">Lebron James</div>
+                                    <div className="feedCardAuthor Tag">@kingjames</div>
+                            </div>
+                            <div className="feedCard-arrow">
+                                    <DownOutlined />
+                            </div>
+                    </div>
+                    <div className="feedCard-body">
+                        We’ve teamed up with @solefed to giveaway a brand new Apple Watch Series 3 (GPS, 42mm) Space Gray Aluminium Case with Black Sport Band to ONE lucky winner. 
+                    </div>
+                    <div className="feedCard-actions">
+                        <HeartOutlined />
+                        <MessageOutlined />
+                    </div>
+                </div>
+            </div>
+            <div className="feedCard">
+                <div className="feedCard-Left">
+                    <div className="profilePicture" style={{backgroundImage: `url(${pic})`, backgroundPosition: "50% 50%", backgroundSize: "cover"}}></div>
+                </div>
+                <div className="feedCard-Right">
+                    <div className="feedCard-header">
+                            <div className="feedCard-info">
+                                    <div className="feedCardAuthor Name">Lebron James</div>
+                                    <div className="feedCardAuthor Tag">@kingjames</div>
+                            </div>
+                            <div className="feedCard-arrow">
+                                    <DownOutlined />
+                            </div>
+                    </div>
+                    <div className="feedCard-body">
+                        We’ve teamed up with @solefed to giveaway a brand new Apple Watch Series 3 (GPS, 42mm) Space Gray Aluminium Case with Black Sport Band to ONE lucky winner. 
+                    </div>
+                    <div className="feedCard-actions">
+                        <HeartOutlined />
+                        <MessageOutlined />
+                    </div>
+                </div>
+            </div>
+            <div className="feedCard">
+                <div className="feedCard-Left">
+                    <div className="profilePicture" style={{backgroundImage: `url(${pic})`, backgroundPosition: "50% 50%", backgroundSize: "cover"}}></div>
+                </div>
+                <div className="feedCard-Right">
+                    <div className="feedCard-header">
+                            <div className="feedCard-info">
+                                    <div className="feedCardAuthor Name">Lebron James</div>
+                                    <div className="feedCardAuthor Tag">@kingjames</div>
+                            </div>
+                            <div className="feedCard-arrow">
+                                    <DownOutlined />
+                            </div>
+                    </div>
+                    <div className="feedCard-body">
+                        We’ve teamed up with @solefed to giveaway a brand new Apple Watch Series 3 (GPS, 42mm) Space Gray Aluminium Case with Black Sport Band to ONE lucky winner. 
+                    </div>
+                    <div className="feedCard-actions">
+                        <HeartOutlined />
+                        <MessageOutlined />
+                    </div>
+                </div>
+            </div>
+            <div className="feedCard">
+                <div className="feedCard-Left">
+                    <div className="profilePicture" style={{backgroundImage: `url(${pic})`, backgroundPosition: "50% 50%", backgroundSize: "cover"}}></div>
+                </div>
+                <div className="feedCard-Right">
+                    <div className="feedCard-header">
+                            <div className="feedCard-info">
+                                    <div className="feedCardAuthor Name">Lebron James</div>
+                                    <div className="feedCardAuthor Tag">@kingjames</div>
+                            </div>
+                            <div className="feedCard-arrow">
+                                    <DownOutlined />
+                            </div>
+                    </div>
+                    <div className="feedCard-body">
+                        We’ve teamed up with @solefed to giveaway a brand new Apple Watch Series 3 (GPS, 42mm) Space Gray Aluminium Case with Black Sport Band to ONE lucky winner. 
+                    </div>
+                    <div className="feedCard-actions">
+                        <HeartOutlined />
+                        <MessageOutlined />
+                    </div>
+                </div>
+            </div>
+            <div className="feedCard">
+                <div className="feedCard-Left">
+                    <div className="profilePicture" style={{backgroundImage: `url(${pic})`, backgroundPosition: "50% 50%", backgroundSize: "cover"}}></div>
+                </div>
+                <div className="feedCard-Right">
+                    <div className="feedCard-header">
+                            <div className="feedCard-info">
+                                    <div className="feedCardAuthor Name">Lebron James</div>
+                                    <div className="feedCardAuthor Tag">@kingjames</div>
+                            </div>
+                            <div className="feedCard-arrow">
+                                    <DownOutlined />
+                            </div>
+                    </div>
+                    <div className="feedCard-body">
+                        We’ve teamed up with @solefed to giveaway a brand new Apple Watch Series 3 (GPS, 42mm) Space Gray Aluminium Case with Black Sport Band to ONE lucky winner. 
+                    </div>
+                    <div className="feedCard-actions">
+                        <HeartOutlined />
+                        <MessageOutlined />
+                    </div>
+                </div>
+            </div>
+
+
+            
+        </div>
     )
 }
 export default ProfileFeed
