0b0bc127d1754878c55797833af7801cb4a2b671
Paul Adu-Gyamfi <pjadu7@gmail.com>
2020-06-28 17:00:10 -0400

    news feed progress

--- client/src/components/views/News/News.js
+++ client/src/components/views/News/News.js
@@ -1,22 +1,88 @@
-import React,{ useContext } from 'react'
+import React,{ useContext, useEffect, useState } from 'react'
 import SideNav from '../Profile/SideNav'
 import Suggestions from '../Profile/Suggestions'
 import { UserContext } from '../../../App'
+import { Link } from 'react-router-dom'
 import './News.scss'
 
 const News = () =>{
 
-
     const {state,dispatch} = useContext(UserContext)
+    const [news,setNews] = useState([])
+    const [category,setCategory] = useState("")
+
+    // NEWS API KEY = f91f0e8b8739477f9fa0f43f787b686e
+
+    let url = 'http://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=f91f0e8b8739477f9fa0f43f787b686e'
+
+    let req = new Request(url);
+
+        useEffect(()=>{
+
+            fetch(req)
+            .then(res=>res.json())
+            .then(result => {
+                console.log(result)
+                setNews(result.articles)
+            })
+
+        },[url])
+
+        const getNews = (category) =>{
+            let newUrl = `https://newsapi.org/v2/everything?q=${category}&` +
+          'apiKey=f91f0e8b8739477f9fa0f43f787b686e'
+
+          let newReq = new Request(newUrl);
+
+            fetch(newReq)
+            .then(res=>res.json())
+            .then(result => {
+                console.log(result)
+                setNews(result.articles)
+            }).catch(err=>{
+                console.log(err)
+            })
+        }
+
+
+    
     return(
     <>
     {state ? 
         <div className="newsContainer">
             <SideNav />
             <div className="middle">
-                NEWS COMING SOON !!!!!!!
+                <div className="newsSections">
+                    <input type='text' onChange={(e)=>setCategory(e.target.value)}/>
+                    <button type="submit" onClick={()=>getNews(category)}>All</button>
+                </div>
+                {
+                    news.map((item,i) => {
+                        return(
+                            
+                            <div className="newsFeedWrap" key={i} onClick={()=>window.open(`${item.url}`)}>
+                                <div className="newsImage"><img src={item.urlToImage} /></div>
+                                    <div className="newsContent">
+                                       <div className="newsHeader">
+                                            <div className='newsHeaderTitle'>{item.title}</div>
+                                            <div className='newsHeaderDate'>{item.publishedAt}</div>
+                                        </div>
+                                        <div className="newsBody">
+                                            {item.author != null || "" ? <div className="newsBodyAuthor">by {item.author}</div>:""}
+                                            <div className="newsBodyText"><p>{item.description}</p></div>
+                                        </div>
+
+                                    </div>
+
+                            </div>
+                           
+                        )
+                    })
+                }
             </div>
-            <Suggestions />
+            {/* <Suggestions /> */}
         </div>
         : null}
     </>
@@ -24,3 +90,39 @@ const News = () =>{
 }
 
 export default News
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+<div className="newsFeedWrap">
+                    <div className="newsImage"><img src='https://cdn.thewirecutter.com/wp-content/uploads/2017/08/picnic-collective-group-lede-630-570x380.jpg' /></div>
+                    <div className="newsHeader">
+                        <div className='newsHeaderTitle'>Great Gear for Picnics and Grilling</div>
+                        <div className='newsHeaderDate'>UPDATED JUNE 26, 2020</div>
+                    </div>
+                    <div className="newsBody">
+                        <div className="newsBodyAuthor">by Wirecutter Staff</div>
+                        <div className="newsBodyText"><p>We spent over 85 hours researching and testing—and selected our favorite tools and gadgets from past guides—to bring you the best gear for picnics and cookouts.</p></div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+*/
---
+++ server/src/routes/news.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router  = express.Router()
+
+
+
+router.get('/news',(req,res) => {
+    res.json()
+})
+
+
+
+
+module.exports = router
