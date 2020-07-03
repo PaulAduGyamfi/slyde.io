5760623551d3bb41f2ccca9325f2810edbadf5df
Paul Adu-Gyamfi <pjadu7@gmail.com>
2020-07-02 21:21:29 -0400

    splash page styling

---
+++ client/src/components/views/Splash/Features/Features.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Features = () => {
+    return(
+        <>
+        <div className="featuresContainer">
+                   <div className="featuresWrap">
+                       <div className="featuresImage"></div>
+                       <div className="featuresDescription">
+                           <div className="title"><h1>Post, Comment & Like</h1></div>
+                           <div className="body"><p>sklcsdncksndcnslcnskdcnlsdcnlscnslcnscnlsdclsnclscnlsdcnlsdcnl</p></div>
+                       </div>
+                   </div>
+               </div>
+
+
+
+               <div className="featuresContainer">
+                   <div className="featuresWrap">
+                       <div className="featuresImage"></div>
+                       <div className="featuresDescription">
+                           <div className="title"><h1>Real-time Messaging</h1></div>
+                           <div className="body"><p>sklcsdncksndcnslcnskdcnlsdcnlscnslcnscnlsdclsnclscnlsdcnlsdcnl</p></div>
+                       </div>
+                   </div>
+               </div>
+        </>
+    )
+}
+
+export default Features
--- client/src/components/views/Splash/Splash.js
+++ client/src/components/views/Splash/Splash.js
@@ -1,5 +1,6 @@
 import React,{useContext} from 'react'
 import { Link } from 'react-router-dom'
+import Features from './Features/Features'
 import './Splash.scss'
 import Dots from '../viewsStyles/imgs/dots.png'
 import logo from '../viewsStyles/imgs/slyde.png'
@@ -12,26 +13,35 @@ const Splash = () => {
            <div className="sectionOne">
                <div className="sectionOneOverlay">
                     <div className="navBar">
-                    <Link to="/">
-                    <img
-            src={logo}
-            alt="logo"
-            style={{ height: "60px", width: "60px"}}
-            draggable="false"
-          />
-                    </Link>
+                        <Link to="/">
+                        <img src={logo} alt="logo" style={{ height: "60px", width: "60px"}} draggable="false" />
+                        </Link>
                    
-    {state?<Link to="/profile" style={{display:'flex', alignItems:'center'}}><span style={{marginRight:'1em'}}>{state.fullname}</span><div className="profilePicture" style={{backgroundImage:`url('${state.pic}')`, backgroundPosition: "50% 50%", backgroundSize: "cover", height:'50px',width:'50px', borderRadius:'50%', marginRight:'4em'}}></div></Link>:<div className="logInOrSignUp"><div className="Login link"><Link to="/signin">Log in</Link></div><div>|</div><div className="Signup link"><Link to="/signup">Sign up</Link></div></div>}
+                        {state?<Link to="/profile" style={{display:'flex', alignItems:'center'}}><span style={{marginRight:'1em'}}>{state.fullname}</span><div className="profilePicture" style={{backgroundImage:`url('${state.pic}')`, backgroundPosition: "50% 50%", backgroundSize: "cover", height:'50px',width:'50px', borderRadius:'50%', marginRight:'4em'}}></div></Link>:<div className="logInOrSignUp"><div className="Login link"><Link to="/signin">Log in</Link></div><div>|</div><div className="Signup link"><Link to="/signup">Sign up</Link></div></div>}
+                    </div>
+                    <div className="sectionTwo">
+                        <div className="sectionTwoOverlay"></div>
+                        <div className="sectionTwoTitle"><h1><span>Slyde</span>, where power of friendship is maintained through distance.</h1></div>
+                        <div className="sectionTwoBody"><p>Don't let social distancing, distance you from current relationships. <span><Link to="/signin">Join Slyde</Link></span> and see what your friends are up to, and make new friends!</p></div>
+                        {/* <Link to="/signin">Connect with friends on Slyde</Link> */}
+                    </div>
+                    <div className="parallaxParagraph">
+                        <div className="parallaxParagraphWrapper">
+                            <div className="parallaxParagraphWrapperBody">
+                                <p>Anyone is welcome.</p>
+                                <p>Great vibes is a must.</p>
+                                <p>Our goal is to not only bring friends together, but all, to be unified as one.</p>
+                                <p>#OneLoveOneHeart</p>
+                            </div>
+                        </div>
                     </div>
                </div>
-               <div className="sectionOneTitle">Slyde</div>
+               
+               <Features />
+               
            </div>
-           <div className="sectionTwo">
-               <div className="sectionTwoOverlay"></div>
-               <div className="sectionTwoTitle"><h2>The power of friendship maintained through distance</h2></div>
-               <div className="sectionTwoBody"><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
-                <div className="sectionTwoLink"><Link to="/signin">Connect with friends on Slyde</Link></div>
-           </div>
+           
+          
        </div>
     )
 }
