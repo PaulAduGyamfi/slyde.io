cb5db38390cdb0cfe31e0a8804e006d90ef88d69
Paul Adu-Gyamfi <pjadu7@gmail.com>
2020-07-04 21:40:47 -0400

    splash styling

--- client/src/components/views/Splash/Features/Features.js
+++ client/src/components/views/Splash/Features/Features.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import POST from '../../viewsStyles/imgs/POST.gif'
+import CHAT from '../../viewsStyles/imgs/CHAT.gif'
 
 const Features = () => {
     return(
         <>
         <div className="featuresContainer">
                    <div className="featuresWrap">
-                       <div className="featuresImage"></div>
+                       <div className="featuresImage" style={{backgroundImage:`url('${POST}')`}}></div>
                        <div className="featuresDescription">
                            <div className="title"><h1>Post, Like & Comment</h1></div>
-                           <div className="body"><p>sklcsdncksndcnslcnskdcnlsdcnlscnslcnscnlsdclsnclscnlsdcnlsdcnl</p></div>
+                           <div className="body"><p>Create an Account today and start creating posts, following others, and even leave a like and or comment under any post.</p></div>
                        </div>
                    </div>
                </div>
@@ -17,10 +19,10 @@ const Features = () => {
 
                <div className="featuresContainer">
                    <div className="featuresWrap">
-                       <div className="featuresImage"></div>
+                       <div className="featuresImage" style={{backgroundImage:`url('${CHAT}')`}}></div>
                        <div className="featuresDescription">
                            <div className="title"><h1>Real Time Messaging</h1></div>
-                           <div className="body"><p>sklcsdncksndcnslcnskdcnlsdcnlscnslcnscnlsdclsnclscnlsdcnlsdcnl</p></div>
+                           <div className="body"><p>Enter a topic or group name and instantly hop into group chats with others that want to talk about the same topic.</p></div>
                        </div>
                    </div>
                </div>
